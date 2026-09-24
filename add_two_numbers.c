04cf4c923cb72e9a3260c66051aca11324112843
VaddeJayanthi <vjayanti689@gmail.com>
2026-09-24 10:42:33 +0530

    Add program to sum two numbers

---
+++ add_two_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Sum = %d\n", a + b);
+
+    return 0;
+}
+ OUTPUT:
+
+Enter two numbers: 43
+45
+Sum = 88
